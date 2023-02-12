38e37519287e43a085a9fcc0b1bd246df75d31dd
Zeynep Altun <20060325@stu.omu.edu.tr>
2023-02-12 18:42:54 +0300

    firebase_init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:store_app/src/views/home_page.dart';
 import 'package:store_app/src/views/login_page.dart';
+import 'package:store_app/src/views/register_page.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,6 +21,7 @@ class MyApp extends StatelessWidget {
       title: "Store App",
       routes: {
         '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignUpPage(),
         '/home': (context) => const HomePage(),
       },
       home: const LoginPage(),
